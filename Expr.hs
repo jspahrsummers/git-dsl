4b8570a21afda4a421181d791eabdc17a3e839b6
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2014-09-02 07:27:54 +0200

    blah

--- Expr.hs
+++ Expr.hs
@@ -5,125 +5,42 @@
 module Expr ( Expr(..)
             ) where
 
-import Prelude ()
+import Data.Monoid
+import Prelude hiding (String)
+import qualified Prelude (String)
 
 data Configuration
 data String
+data Array a
+data Remote
 
 data Expr a where
-    Value :: a -> Expr a
+    Value :: Exportable a => a -> Expr a
     LoadConfig :: Expr Configuration
     ReadConfigKey :: Expr Configuration -> Expr String
+    ListRemotes :: Expr Configuration -> Expr (Array Remote)
 
+newtype Exported a = Exported Prelude.String
 
-instance Functor Expr where
-    fmap f (Value a) = Value $ f a
-    fmap f GetRepository = Value $ f Repository
-    fmap f GetConfiguration =
+instance Functor Exported where
+    fmap f (Exported str) = Exported $ f str
 
-forGreatJustice :: Expr Configuration
-forGreatJustice = GetRepository *> GetConfiguration
+instance Monoid Exported where
+    mempty = Exported mempty
+    mappend a b = Exported $ mappend a b
 
-instance Category Expr where
-    id = Identity
-    b . a = Compose a b
+class Exportable a where
+    export :: a -> Exported a
 
-newtype GeneratedExpr a = GeneratedExpr String
-    deriving (Eq, Show)
+instance Exportable Integer where
+    export n = Exported $ "[RACSignal return:@(" ++ show n ++ ")]"
 
-runGeneratedExpr :: GeneratedExpr a -> String
-runGeneratedExpr (GeneratedExpr str) = str
+instance Exportable Double where
+    export n = Exported $ "[RACSignal return:@(" ++ show n ++ ")]"
 
-apply :: Expr a b -> GeneratedExpr a -> GeneratedExpr b
-apply Empty _ = GeneratedExpr ""
-apply (StringExpr str) _ = GeneratedExpr $ "[RACSignal return:@\"" ++ str ++ "\"]"
-apply Identity input = input
-apply (Compose first second) input = GeneratedExpr $ "[" ++ runGeneratedExpr (apply first input) ++ " flattenMap:^(id input) { return " ++ runGeneratedExpr (apply second $ GeneratedExpr "input")
-apply GetRepository _ = GeneratedExpr $ "self"
-apply GetConfiguration repo = GeneratedExpr $ "[" ++ runGeneratedExpr repo ++ " loadConfiguration]"
+instance Exportable Prelude.String where
+    export str = Exported $ "[RACSignal return:@\"" ++ str ++ "\"]"
 
-doThing = GetRepository >>> GetConfiguration
-
-{-
-newtype GeneratedExpr = String
-
-generate :: Expr a b -> State GeneratedExpr ()
-generate (Value a) =
-generate Identity =
--}
-
-{-
-data Expr a where
-    Value :: a -> Expr a
-    GetRepository :: Expr Repository
-
-instance Functor Expr where
-    fmap f (Value a) = Value $ f a
-    fmap f GetRepository = Value $ f Repository
-
-instance Applicative Expr where
-    pure = Value
-
-    (Value f) <*> (Value a) = Value $ f a
-    (Value f) <*> GetRepository = Value $ f Repository
-
-instance Monad Expr where
-    return = pure
-
-    (Value a) >>= f = f a
-    GetRepository >>= f = Repository
-
-repository :: Expr Repository
-repository = GetRepository
-
-configuration :: Repository -> Expr Configuration
-
-valueForKey :: Configuration -> String -> Expr ConfigValue
-
-remotes :: Configuration -> Expr RemoteList
--}
-
-
-
-
-{-
-data Expr a b where
-    Value :: Gen v => v -> Expr () v
-    Identity :: Expr a a
-    Compose :: Expr a b -> Expr b c -> Expr a c
-
-instance Category Expr where
-    id = Identity
-    bc . ab = Compose ab bc
-
-data Context = Context
-    { repository :: Repository
-    , input :: Identifier
-    } deriving (Eq, Show)
-
-class Gen a where
-    generate :: a -> Reader Context String
-
-newtype Identifier = Identifier String
-
-instance Gen Identifier where
-    generate x = return x
-
-instance Gen Integer where
-    generate n = return "[RACSignal return:@(" ++ show n ++ ")]"
-
-instance Gen Double where
-    generate n = return "[RACSignal return:@(" ++ show n ++ ")]"
-
-instance Gen String where
-    generate s = return "[RACSignal return:@\"" ++ s ++ "\"]"
-
-instance Gen (Expr a b) where
-    generate (Value v) = generate v
-    generate Identity = generate . input <$> ask
-    generate (Compose ea eb) = do
-        ia <- input <$> ask
-        expr <- withReader { input = Identifier "value" } $ generate eb
-        return $ "[" ++ ia ++ " flattenMap:^(id value) { return " ++ expr ++ " }]"
-
--}
+generate :: Expr a -> Exported a
+generate (Value v) = export v
+generate LoadConfig = 
